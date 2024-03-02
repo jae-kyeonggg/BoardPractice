143c1c0c261b3c76dc43f0685ff9bb6a92858ac1
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-02 18:18:46 +0900

    Post entity

---
+++ src/main/java/jae_kyeonggg/board/domain/Post.java
@@ -0,0 +1,28 @@
+package jae_kyeonggg.board.domain;
+
+import jae_kyeonggg.board.config.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity @Getter @Builder
+@AllArgsConstructor
+@Table(name = "posts")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String title;
+    @Column(length = 500, nullable = false)
+    private String content;
+    private String writer;
+    @ColumnDefault("0")
+    private int views;
+
+    public void edit(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
