d27402963324649599daed2d17f1fc527a25783e
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-02 18:12:50 +0900

    설정들 변경

---
+++ src/main/java/jae_kyeonggg/board/domain/User.java
@@ -0,0 +1,48 @@
+package jae_kyeonggg.board.domain;
+
+import jae_kyeonggg.board.config.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity @Getter @Builder
+@AllArgsConstructor
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true) @Email
+    private String email;
+    private String name;
+    @Column(unique = true)
+    private String nickname;
+
+    @OneToMany(mappedBy = "authUser", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Authority> roles = new ArrayList<>();
+
+    public void edit(String name, String nickname) {
+        this.name = name;
+        this.nickname = nickname;
+    }
+
+    public User updateNickName(String nickname) {
+        this.nickname = nickname;
+        return this;
+    }
+
+    public User updateName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setRoles(List<Authority> role) {
+        this.roles = role;
+        role.forEach(o -> o.setUser(this));
+    }
+}
