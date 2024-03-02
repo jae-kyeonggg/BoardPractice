110d6f9dbaf307db40fa266988481185f2c6ba3d
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-02 18:19:05 +0900

    Post 수정 dto

---
+++ src/main/java/jae_kyeonggg/board/domain/dto/info/EditPostInfo.java
@@ -0,0 +1,11 @@
+package jae_kyeonggg.board.domain.dto.info;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter @AllArgsConstructor
+public class EditPostInfo {
+
+    private String title;
+    private String content;
+}
---
+++ src/main/java/jae_kyeonggg/board/domain/dto/request/EditPostRequest.java
@@ -0,0 +1,16 @@
+package jae_kyeonggg.board.domain.dto.request;
+
+import jae_kyeonggg.board.domain.dto.info.EditPostInfo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter @NoArgsConstructor
+public class EditPostRequest {
+
+    private String title;
+    private String content;
+
+    public EditPostInfo toDomain() {
+        return new EditPostInfo(title, content);
+    }
+}
