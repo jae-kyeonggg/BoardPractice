65c17efce490373b473ad88689af761767e80b03
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-06 11:16:05 +0900

    변수 final 설정

--- src/main/java/jae_kyeonggg/board/config/oauth/CustomAuthorityUtils.java
+++ src/main/java/jae_kyeonggg/board/config/oauth/CustomAuthorityUtils.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Component
 public class CustomAuthorityUtils {
 
-    private static List<String> adminMailAddress = List.of("hanjaekyung56@gmail.com", "qwertyuiop26338884@gmail.com");
+    private static final List<String> adminMailAddress = List.of("hanjaekyung56@gmail.com", "qwertyuiop26338884@gmail.com");
 
     private final List<Authority> ADMIN_ROLES = Collections.singletonList(Authority.builder().name("ROLE_ADMIN").build());
     private final List<Authority> USER_ROLES = Collections.singletonList(Authority.builder().name("ROLE_USER").build());
