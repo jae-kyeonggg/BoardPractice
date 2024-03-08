49892b17d81568713cb67e9a4979381140192388
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-08 21:09:02 +0900

    handler impl 변경

--- src/main/java/jae_kyeonggg/board/handler/OAuth2UserSuccessHandler.java
+++ src/main/java/jae_kyeonggg/board/handler/OAuth2UserSuccessHandler.java
@@ -1,25 +1,19 @@
 package jae_kyeonggg.board.handler;
 
 import jae_kyeonggg.board.config.oauth.CustomAuthorityUtils;
-import jae_kyeonggg.board.domain.Authority;
 import jae_kyeonggg.board.domain.CustomOAuth2User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
-public class OAuth2UserSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class OAuth2UserSuccessHandler implements AuthenticationSuccessHandler {
 
     private final CustomAuthorityUtils authorityUtils;
 
@@ -30,10 +24,10 @@ public class OAuth2UserSuccessHandler extends SimpleUrlAuthenticationSuccessHand
         String email = oAuth2User.getEmail();
         authorityUtils.createAuthorities(email);
 
-        redirect(request, response);
+        redirect(response);
     }
 
-    private void redirect(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        getRedirectStrategy().sendRedirect(request, response, "/");
+    private void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/");
     }
 }
