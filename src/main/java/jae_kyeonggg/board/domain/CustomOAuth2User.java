c846ee3b63007027d679a7e0b077555e54035dab
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-11 09:56:20 +0900

    SecurityContextHolder 에 권한이 등록되어 있지 않았던 점 수정

--- src/main/java/jae_kyeonggg/board/domain/CustomOAuth2User.java
+++ src/main/java/jae_kyeonggg/board/domain/CustomOAuth2User.java
@@ -3,12 +3,14 @@ package jae_kyeonggg.board.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class CustomOAuth2User implements OAuth2User, Serializable {
@@ -27,7 +29,9 @@ public class CustomOAuth2User implements OAuth2User, Serializable {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.roles.stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
