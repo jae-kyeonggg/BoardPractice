2c3d2cb38115a025f236232c8951d50f4af38ed4
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-11 11:37:59 +0900

    유저 정보 수정

--- src/main/java/jae_kyeonggg/board/controller/UserController.java
+++ src/main/java/jae_kyeonggg/board/controller/UserController.java
@@ -2,12 +2,10 @@ package jae_kyeonggg.board.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jae_kyeonggg.board.domain.dto.request.EditUserRequest;
-import jae_kyeonggg.board.domain.dto.response.EditUserResponse;
 import jae_kyeonggg.board.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +16,9 @@ public class UserController {
     private final UserService userService;
 
     @Operation(summary = "회원 정보 수정")
-    @PatchMapping("/user/edit/{id}")
-    public ResponseEntity<EditUserResponse> edit(@PathVariable(name = "id") Long userId, @RequestBody EditUserRequest request) {
-        return ResponseEntity.ok().body(userService.edit(userId, request.toDomain()));
+    @PostMapping("/user/edit/{id}")
+    public String edit(@PathVariable(name = "id") Long userId, @RequestBody EditUserRequest request) {
+        userService.edit(userId, request.toDomain());
+        return "redirect:/user/edit/" + userId;
     }
 }
